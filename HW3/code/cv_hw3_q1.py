9cd1dc6f60bb9a77b90599423f8d74df300b5675
ngelfer <nadavgelfer@gmail.com>
2020-05-25 22:05:35 +0300

    finished q1, need to add functions documentation and some tidying up

--- HW3/code/cv_hw3_q1.py
+++ HW3/code/cv_hw3_q1.py
@@ -7,6 +7,7 @@ import torchvision.transforms   as transforms
 import torch.nn                 as nn
 import os 
 import matplotlib.pyplot        as plt
+import matplotlib.cm as cm
 import numpy                    as np
 import cv2
 from PIL                        import Image, ImageFilter
@@ -33,8 +34,21 @@ NORM_MEAN           = [0.485, 0.456, 0.406]
 NORM_STD            = [0.229, 0.224, 0.225]
 # model constants
 DEEP_LAB_CLASSES    = 21
+GRABCUT_CLASSES     = 2     # grabcut gives either background or foreground
+# grabcut dictionary
+GRABCUT_RECT_DICT   = {
+    'frog1.jpg'     : (100,  71, 294, 270),
+    'frog2.jpg'     : (148, 128, 192, 167),
+    'horse1.png'    : ( 23,  10, 870, 554),
+    'horse2.jpg'    : (577, 164, 438, 528),
+    'q3_cat.jpeg'   : (  0,  24, 493, 491),
+    'q3_kindle.jpg' : ( 76,  67, 362, 368),
+    'q3_tv.jpg'     : ( 83,  63, 179, 123)
+}
+GRABCUT_ITER        = 5
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
 
 
 ###############################################################################
@@ -59,7 +73,7 @@ transform_vgg = transforms.Compose(
 ###############################################################################
 #                            functions                                        #
 ###############################################################################
-def load_images(fp):
+def load_images(fp, named = False, method = 'PIL'):
     # desc
     # recieves a path to a dir of images and load them all to a list
     #
@@ -67,21 +81,62 @@ def load_images(fp):
     # fp - path to dir
     #
     # output
-    # im_list - listen of PIL.Image objects
+    # im_list - list of PIL.Image objects
     img_list = []
-    for img in os.listdir(fp):
-        fp_img = os.path.join(fp, img)
-        img_obj = Image.open(fp_img)
-        img_list.append(img_obj)
+    for fn_img in os.listdir(fp):
+        fp_img = os.path.join(fp, fn_img)
+        if method == 'cv':
+            img_obj = cv2.imread(fp_img)
+            img_obj = cv2.cvtColor(img_obj, cv2.COLOR_BGR2RGB)
+        elif method == 'PIL':
+            img_obj = Image.open(fp_img)
+        else:
+            print('bad image method')
+            return None
+        if named:
+            img_list.append((fn_img, img_obj))
+        else:
+            img_list.append(img_obj)
     return img_list
 
 
-def blur_images(img_list):
-    blurred = []
-    for img in img_list:
-        blr_img = img.filter(ImageFilter.BLUR)
-        blurred.append(blr_img)
-    return blurred
+def wrapper_grabcut(named_img_list, colors_palette):
+    seg = []
+    masks = []
+
+    for fn, img in named_img_list:
+        mask        = np.zeros(img.shape[:2], np.uint8)
+        bgd_model   = np.zeros((1, 65), np.float64)
+        fgd_model   = np.zeros((1, 65), np.float64)
+        rct         = GRABCUT_RECT_DICT[fn]
+        cv2.grabCut(img, mask, rct, bgd_model, fgd_model, GRABCUT_ITER, cv2.GC_INIT_WITH_RECT)
+        mask2 = np.where((mask == 2) | (mask == 0), 0, 1).astype('uint8')
+        mask_img = Image.fromarray(mask2)
+        mask_img.putpalette(colors_palette)
+        masked_img = img * mask2[:, :, np.newaxis]
+        seg.append(masked_img)
+        masks.append(mask_img)
+
+    return seg, masks
+
+
+def plot_classic_segments(seg_list, img_label):
+
+    num_im = len(seg_list)
+    _, axes = plt.subplots(1, num_im)
+    if num_im == 1:
+        img = seg_list[0]
+        axes.imshow(img)
+        axes.set_title(img_label + ': masked img')
+        axes.set_xticks([])
+        axes.set_yticks([])
+    else:
+        for i, img in enumerate(seg_list):
+            axes[i].imshow(img)
+            axes[i].set_title(img_label + ': seg. for img #' + str(i + 1))
+            axes[i].set_xticks([])
+            axes[i].set_yticks([])  
+    plt.show()
 
 
 
@@ -282,6 +337,22 @@ def plot_prediction(pred_list):
     plt.show()
 
 
+
+
+def paste_image(front, bg, os, mask, resize):
+    if resize is not False:
+        front.thumbnail(resize)
+        mask.thumbnail(resize)
+    bg.paste(front, os, mask)
+    return bg
+
+
+def binary_mask(img, mask, colors_palette):
+    img_mask = Image.fromarray(mask.byte().cpu().numpy()).resize(img.size)
+    img_mask.putpalette(colors_palette)
+    bw_mask = img_mask.convert('1')
+    return bw_mask
+
 ###############################################################################
 #                                main                                         #
 ###############################################################################
@@ -294,63 +365,92 @@ def main():
     fp_horses = os.path.join(root, DIR_DATA, SUBDIR_HORSES)
     img_frogs = load_images(fp_frogs)
     img_horses = load_images(fp_horses)
+    img_named_frogs = load_images(fp_frogs, named = True, method = 'cv')
+    img_named_horses = load_images(fp_horses, named = True, method = 'cv')
+
+    deeplab_palette = get_palette(DEEP_LAB_CLASSES)
+    grabcut_palette = get_palette(GRABCUT_CLASSES)
+
 
     # SUBQUESTION 1.2 - CLASSIC METHOD
-    # TODO: not implemented yet
-    
+    #classic_frogs_seg, classic_frogs_masks = wrapper_grabcut(img_named_frogs, grabcut_palette)
+    #classic_horse_seg, classic_horse_masks = wrapper_grabcut(img_named_horses, grabcut_palette)
+    #plot_classic_segments(classic_frogs_seg, 'frogs')
+    #plot_classic_segments(classic_frogs_masks, 'frogs')
+    #plot_classic_segments(classic_horse_masks, 'horses')
+    #plot_classic_segments(classic_horse_seg, 'horses')
+
     
     # SUBQUESTION 1.2 - DEEP LEARNING BASED METHOD
-    model = torch.hub.load('pytorch/vision:v0.5.0', 'deeplabv3_resnet101', pretrained = True)
-    model.eval()
-    model = model.to(device)
-    deeplab_palette = get_palette(DEEP_LAB_CLASSES)
-    #pred_frogs = predict_segment(model, img_frogs)
+    model_deeplab = torch.hub.load('pytorch/vision:v0.5.0', 'deeplabv3_resnet101', pretrained = True)
+    model_deeplab.eval()
+    model_deeplab = model_deeplab.to(device)
+    #pred_frogs = predict_segment(model_deeplab, img_frogs)
     #plot_segments(pred_frogs, deeplab_palette, 'frogs')
     #plot_object(pred_frogs, 'frogs')
-    #pred_horses = predict_segment(model, img_horses)
+    #pred_horses = predict_segment(model_deeplab, img_horses)
     #plot_segments(pred_horses, deeplab_palette, 'horses')
     #plot_object(pred_horses, 'horses')
 
-    # SUBQUESTION 1.3
-    # fp_rnd_images = os.path.join(root, DIR_MY_DATA, SUBDIR_RAND_IMAGES)
-    # img_rnd_images = load_images(fp_rnd_images)
-    # pred_rnd = predict_segment(model, img_rnd_images)
-    # plot_segments(pred_rnd, deeplab_palette, 'SQ3')
-    # plot_object(pred_rnd, 'SQ3')
+    # SUBQUESTION 1.3 and 1.4
+    #fp_rnd_images = os.path.join(root, DIR_MY_DATA, SUBDIR_RAND_IMAGES)
+    #img_rnd_images = load_images(fp_rnd_images)
+    #img_named_rnd_images = load_images(fp_rnd_images, named = True, method = 'cv')
+    #classic_rnd_seg, classic_rnd_masks = wrapper_grabcut(img_named_rnd_images, grabcut_palette)
+    #plot_classic_segments(classic_rnd_seg, 'SQ3')
+    #plot_classic_segments(classic_rnd_masks, 'SQ3')
+    
+    #pred_rnd = predict_segment(model_deeplab, img_rnd_images)
+    #plot_segments(pred_rnd, deeplab_palette, 'SQ3')
+    #plot_object(pred_rnd, 'SQ3')
 
 
     # SUBQUESTION 1.5
     # TODO: find pre or post processing to improve edges
 
     # SUBQUESTION 1.6
-    #model = torchvision.models.vgg16(pretrained = True, progress = True)
-    #model.eval()
-    #model = model.to(device)
+    model_vgg16 = torchvision.models.vgg16(pretrained = True, progress = True)
+    model_vgg16.eval()
+    model_vgg16 = model_vgg16.to(device)
 
 
     # SUBQUESTION 1.7
     fp_cow = os.path.join(root, DIR_DATA, IMG_COW)
-    img_cow = Image.open(fp_cow)    
+    img_cow = Image.open(fp_cow) 
     #fp_sheep = os.path.join(root, DIR_DATA, IMG_SHEEP)
     #img_sheep = Image.open(fp_sheep)
     #img_list = [img_cow, img_sheep]
-    #pred_animals = predict_class(model, img_list, fp_labels)
+    #pred_animals = predict_class(model_vgg16, img_list, fp_labels)
     #plot_prediction(pred_animals)
 
     # SUBQUESTION 1.8
-    #pred_cow = predict_segment(model, [img_cow])
+    #pred_cow = predict_segment(model_deeplab, [img_cow])
     #plot_object(pred_cow, 'cow')
 
     # SUBQUESTION 1.9
-    #fp_beach = os.path.join(root, DIR_DATA, IMG_BEACH)
-    #img_beach = Image.open(fp_beach) 
-    ## we resize the cow to fit in the beach image   
-    #img_cow.thumbnail(size = (250, 250))
-    #pred_cow = predict_segment(model, [img_cow])
-    #masked_cow = create_obj_mask(pred_cow)
-#
-    #img_beach.paste(img_cow, (300, 150))
-    #img_beach.save('mod_beach.jpg')
+    fp_beach = os.path.join(root, DIR_DATA, IMG_BEACH)
+    img_beach = Image.open(fp_beach) 
+    pred_cow = predict_segment(model_deeplab, [img_cow])
+    _, cow_mask = pred_cow[0]
+    binary_mask_cow = binary_mask(img_cow, cow_mask, deeplab_palette)
+    cow_on_the_beach = paste_image(
+        front = img_cow, 
+        bg = img_beach, 
+        os = (300, 150),
+        mask = binary_mask_cow,
+        resize = (250, 250)
+    )
+    _, axes = plt.subplots(1, 1)
+    axes.imshow(cow_on_the_beach)
+    axes.set_xticks([])
+    axes.set_yticks([])
+    plt.show()
+
+    # SUBQUESTION 1.10
+    pred_beach_cow = predict_class(model_vgg16, [cow_on_the_beach], fp_labels)
+    plot_prediction(pred_beach_cow)
+
+
 
 
 if __name__ == "__main__":
